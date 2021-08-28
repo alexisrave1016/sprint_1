2694b56e52ec3f3b8ccf86b85c148b224bd36d66
alexis <kiarepuestos.medellin@gmail.com>
2021-08-28 15:12:03 -0500

    actualizacion funciones

--- script/preg_sel.js
+++ script/preg_sel.js
@@ -2,14 +2,22 @@ let progreso = 0;
 const longitudBarra = 240;
 let dataDb = [];
 let contadorPreguntas = 0;
-const respuestasTotales = document.getElementById('respuestasTotales')
-const pregunta = document.getElementById('pregunta_aleatorio')
+const respuestasTotales = document.getElementById('respuestasTotales');
+const pregunta = document.getElementById('pregunta_aleatorio');
 const respuesta1 = document.querySelector('.respuesta1');
 const respuesta2 = document.querySelector('.respuesta2');
 const respuesta3 = document.querySelector('.respuesta3');
+// empezamos a declarar otro tipo de pregunta
+// const respuestaImg = document.querySelector('respuesta_imagenes');
+const imagen1 = document.querySelector('.imagen1');
+const imagen2 = document.querySelector('.imagen2');
+const imagen3 = document.querySelector('.imagen3');
+const imagen4 = document.querySelector('.imagen4');
+
+
 
 const verificarRespuesta = (evento) => {
-    console.log(evento);
+    // console.log(evento);
     const respuestaCorrecta = dataDb[contadorPreguntas].respuesta_correcta;
     if (evento.target.innerText === respuestaCorrecta) {
         contadorPreguntas = contadorPreguntas + 1;
@@ -22,6 +30,13 @@ const verificarRespuesta = (evento) => {
 respuesta1.addEventListener('click', verificarRespuesta);
 respuesta2.addEventListener('click', verificarRespuesta);
 respuesta3.addEventListener('click', verificarRespuesta);
+
+//damos evento a la imagenes
+
+imagen1.addEventListener('click', verificarRespuesta);
+imagen2.addEventListener('click', verificarRespuesta);
+imagen3.addEventListener('click', verificarRespuesta);
+imagen4.addEventListener('click', verificarRespuesta);
 
 const aumentarProgreso = () => {
     const avance = 240 / 6;
@@ -42,25 +57,47 @@ const aumentarProgreso = () => {
 const cargaRespuestas = () => {
     console.log(contadorPreguntas, dataDb);
     pregunta.innerText = dataDb[contadorPreguntas].pregunta;
-  
-    respuesta1.innerText= dataDb[contadorPreguntas].respuestas[0];
-    respuesta2.innerText= dataDb[contadorPreguntas].respuestas[1];
-    respuesta3.innerText= dataDb[contadorPreguntas].respuestas[2];
+
+    respuesta1.innerText = dataDb[contadorPreguntas].respuestas[0];
+    respuesta2.innerText = dataDb[contadorPreguntas].respuestas[1];
+    respuesta3.innerText = dataDb[contadorPreguntas].respuestas[2];
     
-    console.log(dataDb[contadorPreguntas]);
+  
+    }
+
+    if(contadorPreguntas.tipo_pregunta===imagen){
+        respuestasTotales.innerHTML
+  
+    // console.log(dataDb[contadorPreguntas]);
+}
+
+    //cargar imagenes
+    // if(dataDb.tipo_pregunta===imagen){
+    //     respuestasTotales.innerHTML=''
+    // }
+    // contenedor_prefuntas.innerHTML ='';
+    // pregunta.innerText = dataDb[contadorPreguntas].pregunta;
+   
+ 
 
 
 
+    // imagen1.innerText = dataDb[contadorPreguntas].respuestas[0];
+    // imagen2.innerText = dataDb[contadorPreguntas].respuestas[1];
+    // imagen4.innerText = dataDb[contadorPreguntas].respuestas[2];
+    // imagen4.innerText = dataDb[contadorPreguntas].respuestas[3];
 
 }
 
 
+//cambio el tipo d efiltro , por categoria
 
 fetch('../data/bdPreguntas.json')
     .then(response => response.json())
     .then(data => {
-        dataDb = data.filter((pregunta) => pregunta.tipo_pregunta === 'seleccion');
+        dataDb = data.filter((pregunta) => pregunta.categoria === 'html');
         //aca cargamos el random
         cargaRespuestas();
+        console.log(dataDB);
     })
     .catch(error => console.log(error));
